03a307690408716dda4329dafc1da81a7dd04ef1
Stefan Machmeier <stefan-machmeier@outlook.com>
2023-01-25 11:45:34 +0100

    Update Producer

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
+[![GitHub Stars](https://img.shields.io/github/stars/stefanDeveloper/heistream)](https://github.com/stefanDeveloper/heistream/) [![Docker Pulls](https://img.shields.io/docker/pulls/stefan96/heistream-ndpid.svg)](https://hub.docker.com/r/stefan96/heistream-ndpid/) [![Docker Stars](https://img.shields.io/docker/stars/stefan96/heistream-ndpid.svg)](https://hub.docker.com/r/stefan96/heistream-ndpid/)
+
 # heiStream
 
 nDPId Docker Image
 
-## Getting Started
+## How to use this image
 
 ```bash
 docker pull 
-docker run -p 127.0.0.1:7000:7000 heistream-producer:latest
+docker run -p 127.0.0.1:7000:7000 -net host stefan96/heistream-ndpid:producer-latest
+docker run -net host stefan96/heistream-ndpid:consumer-latest
 ```
+
+## 
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,19 +2,42 @@
 
 # exit script if any command fails (non-zero value)
 set -e
+params_ndpisrvd=()
+params_ndpid=()
+
+# Create params for nDPIsrvd
+
+[[ $MAX_BUFFERED_LINES -gt 0 ]] && params_ndpisrvd+=(-C $MAX_BUFFERED_LINES)
+
+# Create params for nDPId
+
+regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
+
+if [[ $JA3_URL =~ $regex ]]; then
+    curl $JA3_URL > /root/ja3_fingerprints.csv
+    params_ndpid+=(-J /root/ja3_fingerprints.csv)
+fi
+
+if [[ $SSL_SHA1_URL =~ $regex ]]; then
+    curl $SSL_SHA1_URL > /root/sslblacklist.csv
+    params_ndpid+=(-S /root/sslblacklist.csv)
+fi
+
+[[ $MAX_THREADS -gt 0 ]] && params_ndpid+=(-C $MAX_THREADS)
+
+[[ $FLOW_ANALYSIS = true ]] && params_ndpid+=(-A)
 
 /root/nDPIsrvd -p /tmp/nDPIsrvd-daemon.pid \
             -c /tmp/nDPIsrvd-daemon-collector.sock \
             -s /tmp/nDPIsrvd-daemon-distributor.sock \
-            -S 0.0.0.0:7000 \
+            -S 0.0.0.0:$PORT \
             -u root \
             -d \
             -L /tmp/nDPIsrvd.log \
-            -C $MAX_BUFFERED_LINES
+            "${params_ndpisrvd[@]}"
 
 exec /root/nDPId -p /tmp/nDPId-daemon.pid \
             -c /tmp/nDPIsrvd-daemon-collector.sock \
             -u root \
             -L /tmp/nDPId.log \
-            -A \
-            -o max-reader-threads=$MAX_THREADS
+            "${params_ndpid[@]}"

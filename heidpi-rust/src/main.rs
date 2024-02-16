7bcc522c314fbbbf823eabee0ad3969a4578e36d
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-02-16 11:07:40 +0100

    First rust draft

--- heidpi-rust/src/main.rs
+++ heidpi-rust/src/main.rs
@@ -2,7 +2,7 @@ use log::{info, trace, warn};
 use log::{Level, Metadata, Record};
 use log::{LevelFilter, SetLoggerError};
 use serde_json::{error, Value};
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Read};
 use std::net::TcpStream;
 use std::str;
 use std::time::Duration;
@@ -10,6 +10,7 @@ use std::{thread, time};
 
 static LOGGER: SimpleLogger = SimpleLogger;
 const NETWORK_BUFFER_LENGTH_DIGITS: usize = 5;
+const NETWORK_BUFFER_MAX_SIZE: usize = 33792;
 
 struct SimpleLogger;
 
@@ -27,7 +28,8 @@ impl log::Log for SimpleLogger {
     fn flush(&self) {}
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let _ = log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Trace));
     loop {
         match TcpStream::connect("127.0.0.1:7000") {
@@ -40,9 +42,12 @@ fn main() {
 
                 continue;
             }
-            Ok(stream) => {
+            Ok(mut stream) => {
                 info!("Connected");
-
+                match stream.set_nonblocking(true) {
+                    Ok(..) => info!("Non-blocking State"),
+                    Err(..) => panic!("Non-blocking State Failed"),
+                };
                 match stream.set_read_timeout(Some(Duration::new(60, 0))) {
                     Ok(..) => info!("Set Read Timeout"),
                     Err(..) => panic!("Setting Read Timeout Failed"),
@@ -53,46 +58,66 @@ fn main() {
                     Err(..) => panic!("Setting Write Timeout Failed"),
                 };
 
+                let mut buf = [0u8; NETWORK_BUFFER_MAX_SIZE];
+
                 loop {
                     trace!("Loop");
 
-                    let mut packet = BufReader::new(&stream);
-                    let mut xml = vec![];
+                    let mut buf_used = 0;
 
-                    match packet.read_until(b'\n', &mut xml) {
-                        Ok(bytes) => {
-                            if bytes == 0 {
-                                // This will break us out back out to the first loop to recreate the connection.
-                                break;
-                            } else {
-                                trace!("Read {:?} Bytes", bytes);
-                                let s = match str::from_utf8(&xml[NETWORK_BUFFER_LENGTH_DIGITS..]) {
-                                    Ok(v) => v,
-                                    Err(e) => {
-                                        warn!("Invalid UTF-8 sequence: '{}', retrun empty string.", e);
-                                        ""
-                                    },
-                                };
-                                let v: Value = match serde_json::from_str(s) {
-                                    Ok(json) => {
-                                        trace!("Converted result: {}", json);
-                                        json
+                    match stream.read(&mut buf[buf_used..]) {
+                        Ok(data) => {
+                            info!("{:?}", data);
+                            info!("Buffer used: {:?}", buf_used);
+                            let json_str_start = match std::str::from_utf8(&buf[buf_used..data]) {
+                                Ok(json_str) => {
+                                    // info!("{:?}", json_str);
+                                    buf_used += data;
+                                    // TODO error handling
+                                    let s = match str::from_utf8(json_str.as_bytes()) {
+                                        Ok(v) => v,
+                                        Err(e) => {
+                                            warn!("Invalid UTF-8 sequence: '{}', retrun empty string.", e);
+                                            ""
+                                        }
+                                    };
+                                    let mut s_split = s.split("\n").into_iter();
+
+                                    for s_plit_n in s_split {
+                                        if s_plit_n.len() > NETWORK_BUFFER_LENGTH_DIGITS {
+                                            let v: Value = match serde_json::from_str(
+                                                &s_plit_n[NETWORK_BUFFER_LENGTH_DIGITS..],
+                                            ) {
+                                                Ok(json) => {
+                                                    info!("Converted result: {}", json);
+                                                    json
+                                                }
+                                                Err(_e) => {
+                                                    panic!(
+                                                        "Invalid JSON object: '{}' for string: {}",
+                                                        _e, s
+                                                    );
+                                                    serde_json::Value::Null
+                                                }
+                                            };
+                                        };
                                     }
-                                    Err(_e) => {
-                                        warn!("Invalid JSON object: '{}' for string: {}", _e, s);
-                                        serde_json::Value::Null
-                                    }
-                                };
-                            }
+                                }
+                                Err(_) => {
+                                    warn!("BUG: Invalid UTF-8 in buffer");
+                                }
+                            };
                         }
-                        Err(ref _e) if _e.kind() == io::ErrorKind::WouldBlock => {
-                            warn!("No data, send heartbeat");
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                             continue;
                         }
-                        Err(e) => {
-                            warn!("Encountered IO error: {}", e.to_string());
+                        Err(..) => {
+                            let five_seconds = time::Duration::from_millis(10);
+                            thread::sleep(five_seconds);
+
+                            continue;
                         }
-                    };
+                    }
                 }
             }
         }

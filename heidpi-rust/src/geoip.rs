35c8135a690c05b7c995bcb5bcdb403178a4864a
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-02-27 12:06:41 +0100

    geoip2 coments

--- heidpi-rust/src/geoip.rs
+++ heidpi-rust/src/geoip.rs
@@ -1,12 +1,18 @@
 use std::net::IpAddr;
+use anyhow::Ok;
 use maxminddb::Reader;
 
 /// .
+// TODO create geoip2 object, be aware multiple threads will read this object, check Tokio how to do it! (Mutex...)
 struct GeoIP {
     
 }
 
+
 impl GeoIP {
+    pub fn new()-> anyhow::Result<()> {
+        Ok(())
+    }
     pub fn get_geoip(ip: &IpAddr) -> anyhow::Result<()> {
         let reader = maxminddb::Reader::open_readfile("test-data/test-data/GeoIP2-City-Test.mmdb").unwrap();
         Ok(())

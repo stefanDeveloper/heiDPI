79f739f8dfe1bacce53f98b4ca6480e22b95986d
Stefan Machmeier <stefan-machmeier@outlook.com>
2024-02-27 11:49:08 +0100

    Handle parameters

--- heidpi-rust/src/cli.rs
+++ heidpi-rust/src/cli.rs
@@ -68,6 +68,7 @@ impl Cli {
                 error_events,
                 flow_events,  
              } => {
+                // TODO Handle parameters
                 let mut v = stream::connect("127.0.0.1:7000").await;
             }
         }
